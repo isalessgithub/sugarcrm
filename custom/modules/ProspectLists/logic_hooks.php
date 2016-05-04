d942b9a117d1d98554e5c7edba4c24230d9e8876
KenMcCartney <ken@kenmccartney.com>
2016-05-04 17:06:21 -0400

    Add Account Contacts to TargetLists

---
+++ custom/modules/ProspectLists/logic_hooks.php
@@ -0,0 +1,6 @@
+<?php
+     $hook_version = 1;
+     $hook_array = Array();
+     $hook_array['before_relationship_add'] = Array();
+     $hook_array['before_relationship_add'][] = Array(1,'Hook description','custom/modules/ProspectLists/addFromTargetList.php','AddFromTargetList','addFromTargetListMethod');
+?>
