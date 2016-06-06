2e6fd0c0a6cfdf1664a96a5d9d149fe4eb8a8047
KenMcCartney <ken@kenmccartney.com>
2016-06-06 13:02:19 -0400

    merge

--- custom/Extension/modules/ATC_ISSCampaigns/Ext/Layoutdefs/atc_isscampaigns_quotes_1_ATC_ISSCampaigns.php
+++ custom/Extension/modules/ATC_ISSCampaigns/Ext/Layoutdefs/atc_isscampaigns_quotes_1_ATC_ISSCampaigns.php
@@ -8,13 +8,13 @@ $layout_defs["ATC_ISSCampaigns"]["subpanel_setup"]['atc_isscampaigns_quotes_1'] 
   'sort_by' => 'id',
   'title_key' => 'LBL_ATC_ISSCAMPAIGNS_QUOTES_1_FROM_QUOTES_TITLE',
   'get_subpanel_data' => 'atc_isscampaigns_quotes_1',
-  'top_buttons' => 
+  'top_buttons' =>
   array (
-    0 => 
+    0 =>
     array (
       'widget_class' => 'SubPanelTopButtonQuickCreate',
     ),
-    1 => 
+    1 =>
     array (
       'widget_class' => 'SubPanelTopSelectButton',
       'mode' => 'MultiSelect',
