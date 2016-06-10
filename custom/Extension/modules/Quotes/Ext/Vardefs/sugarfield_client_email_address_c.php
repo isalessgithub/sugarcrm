42055042025bbe8447fcf2f26ad4047c7c2c506d
root <root@ip-172-31-53-184.ec2.internal>
2016-06-10 02:47:56 +0000

    Update for client email address logic hook for Quotes

---
+++ custom/Extension/modules/Quotes/Ext/Vardefs/sugarfield_client_email_address_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2016-06-09 10:44:50
+$dictionary['Quote']['fields']['client_email_address_c']['enforced']='';
+$dictionary['Quote']['fields']['client_email_address_c']['dependency']='';
+
+ ?>
