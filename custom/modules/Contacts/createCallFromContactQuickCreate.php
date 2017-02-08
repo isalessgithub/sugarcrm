408938258b77563408f96a6d2f94a45cd4d78595
root <root@ip-172-31-53-184.ec2.internal>
2017-02-08 03:33:24 +0000

    feb 7

--- custom/modules/Contacts/createCallFromContactQuickCreate.php
+++ custom/modules/Contacts/createCallFromContactQuickCreate.php
@@ -3,27 +3,40 @@
 class createCallFromContactQuickCreateC {
 
     function createCallFromContactQuickCreateBeforeSave($bean, $action, $args) {
-        global $current_user;
-        if (empty($bean->fetched_row)) {
-            if (isset($_REQUEST['call_outcome_c']) && isset($_REQUEST['relate_to']) && $_REQUEST['relate_to'] == 'prospect_list_contacts') {
-                $bean->description .= "\n" . date('m/d/Y h:ia') . " * Call Made by " . $current_user->full_name . " * ";
-            }
-        }
+	if($args->isUpdate === 'false'){
+        	global $current_user;
+		//$GLOBALS['log']->fatal("--".json_encode($args, JSON_PRETTY_PRINT));
+                $query = "SELECT prospect_list_id from prospect_lists_prospects where related_id = '{$bean->id}' AND deleted=0 order by date_modified desc limit 1";
+         	$target_list_id = $GLOBALS['db']->getOne($query);
+		//$GLOBALS['log']->fatal("**".$target_list_id);
+        	if (!empty($target_list_id)) {
+                		$bean->description .= "\n" . date('m/d/Y h:ia') . " * Call Made by " . $current_user->full_name . " * ";
+	 	}
+     	}
     }
 
     function createCallFromContactQuickCreateAfterSave($bean, $action, $args) {
-        if (empty($bean->fetched_row)) {
-            if (isset($_REQUEST['call_outcome_c']) && isset($_REQUEST['relate_to']) && $_REQUEST['relate_to'] == 'prospect_list_contacts') {
+//	$GLOBALS['log']->fatal("++".$bean->id);
+
+        if ($args['isUpdate'] == false){
+
+
+		$query = "SELECT prospect_list_id from prospect_lists_prospects where related_id = '{$bean->id}' AND deleted=0 order by date_modified desc limit 1";
+		$target_list_id = $GLOBALS['db']->getOne($query);
+
+            if ($target_list_id != '' && isset($target_list_id)) {
+		$GLOBALS['log']->fatal('generating call related to new contact');
+
                 global $current_user;
                 $call = new Call();
                 $module = 'Contacts';
                 $parent_id = $bean->id;
 
-                $call->name = "Left Voicemail";
-                $call_outcome_c = $_REQUEST['call_outcome_c'];
+                $call->name = "First Call";
+                $call_outcome_c = $bean->call_outcome_c;
 
                 $call->call_outcome_c = $call_outcome_c;
-
+		$call->description = $bean->description;
                 $call->assigned_user_id = $current_user->id;
                 $call->assigned_user_name = $current_user->full_name;
                 $call->direction = "Outbound";
@@ -34,17 +47,18 @@ class createCallFromContactQuickCreateC {
 
                 $call->name = "Made call to " . $bean->full_name;
                 // lets relate the call to Target list
-                if (!empty($_REQUEST['return_id']))
-                    $call->prospectlists_calls_1prospectlists_ida = $_REQUEST['return_id'];
+                if (!empty($target_list_id))
+
+                $call->prospectlists_calls_1prospectlists_ida = $target_list_id;
+
                 $_SESSION['disable_workflow'] = "Yes";
                 $call->save();
                 $tl = new ProspectList();
                 $tl->disable_row_level_security = true;
-                $tl->retrieve($_REQUEST['return_id']);
-                global $db;
+                $tl->retrieve($target_list_id);
 
                 $update_campaign_name_c_query = "UPDATE calls_cstm SET campaign_name_c = '{$tl->name}' WHERE id_c = '{$call->id}'";
-                $db->query($update_campaign_name_c_query);
+                $GLOBALS['db']->query($update_campaign_name_c_query);
 
                 $_SESSION['disable_workflow'] = "No";
                 $columns = array("call_id" => $call->id, "contact_id" => $parent_id);
