1aab4b0b76b0c2ab0a81f71d79d5ce814f22fb11
Milos Kovacevic <mkovacevic@eontek.co>
2017-02-28 12:43:13 +0100

    vendor folder

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,17 +6,27 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Symfony\\Component\\Validator\\' => array($vendorDir . '/symfony/validator'),
     'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
     'Symfony\\Component\\Security\\Csrf\\' => array($vendorDir . '/symfony/security-csrf'),
     'Symfony\\Component\\Security\\Core\\' => array($vendorDir . '/symfony/security-core'),
+    'Symfony\\Component\\Process\\' => array($vendorDir . '/symfony/process'),
+    'Symfony\\Component\\Finder\\' => array($vendorDir . '/symfony/finder'),
     'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Sugarcrm\\Sugarcrm\\inc\\' => array($baseDir . '/include'),
     'Sugarcrm\\Sugarcrm\\custom\\inc\\' => array($baseDir . '/custom/include'),
     'Sugarcrm\\Sugarcrm\\custom\\' => array($baseDir . '/custom/src', $baseDir . '/custom'),
     'Sugarcrm\\Sugarcrm\\' => array($baseDir . '/src', $baseDir . '/'),
+    'Sugarcrm\\SugarcrmTests\\inc\\' => array($baseDir . '/tests/include'),
+    'Sugarcrm\\SugarcrmTests\\' => array($baseDir . '/tests/src', $baseDir . '/tests'),
+    'Sugarcrm\\SugarcrmTestsUnit\\inc\\' => array($baseDir . '/testsunit/include'),
+    'Sugarcrm\\SugarcrmTestsUnit\\' => array($baseDir . '/testsunit/src', $baseDir . '/testsunit'),
+    'Seld\\JsonLint\\' => array($vendorDir . '/seld/jsonlint/src/Seld/JsonLint'),
     'Rhumsaa\\Uuid\\' => array($vendorDir . '/ramsey/uuid/src'),
+    'JsonSchema\\' => array($vendorDir . '/justinrainbow/json-schema/src/JsonSchema'),
     'Elastica\\Test\\' => array($vendorDir . '/ruflin/elastica/test/lib/Elastica/Test'),
     'Elastica\\' => array($vendorDir . '/ruflin/elastica/lib/Elastica'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
 );
