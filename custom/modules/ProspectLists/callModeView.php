44d94c1a42391fe19cd8e593e23b8e17f7526d3a
root <root@localhost.localdomain>
2017-04-11 12:11:23 -0400

    modified callmode view

--- custom/modules/ProspectLists/callModeView.php
+++ custom/modules/ProspectLists/callModeView.php
@@ -4,6 +4,12 @@ echo getVersionedScript('modules/ATC_Teleseller/javascript/ATC_JS/call_logging.j
 
 $url = "http://test.isaless.com/";
 global $mod_strings;
+global $current_language;
+global $sugar_config;
+
+//global $db;
+$db = $GLOBALS['db'];
+
 // only if Target ID exists
 if(isset($_REQUEST['uid']) && !empty($_REQUEST['uid'])){
 $sTargetId = $_REQUEST['uid'];
@@ -19,13 +25,12 @@ echo getClassicModuleTitle("ProspectLists", array("<a href='index.php?module=Pro
 
 // fetch all related contacts
 //$oContacts = $oProspectList->get_linked_beans('contacts', 'Contacts');
-//global $db;
-$db = $GLOBALS['db'];
 
-$cSQL = " SELECT accounts.id 'account_id', accounts.name 'account_name', CONCAT(first_name, ' ', last_name) AS 'full_name', contacts.id AS 'contact_id', title,phone_other, phone_work, primary_address_city, primary_address_state, call_outcome_c, email_addresses.email_address as 'email1' 
+
+$cSQL = " SELECT accounts.id 'account_id', accounts.name 'account_name', CONCAT(first_name, ' ', last_name) AS 'full_name', contacts.id AS 'contact_id', title,phone_other, phone_work, primary_address_city, primary_address_state, call_outcome_c, email_addresses.email_address as 'email1'
 FROM contacts
 INNER JOIN contacts_cstm ON contacts_cstm.id_c = contacts.id AND contacts.deleted = 0
-INNER JOIN prospect_lists_prospects ON prospect_lists_prospects.related_id = contacts.id AND prospect_list_id = '".$_REQUEST['uid']."' AND prospect_lists_prospects.deleted=0 AND prospect_lists_prospects.related_type ='Contacts' 
+INNER JOIN prospect_lists_prospects ON prospect_lists_prospects.related_id = contacts.id AND prospect_list_id = '".$_REQUEST['uid']."' AND prospect_lists_prospects.deleted=0 AND prospect_lists_prospects.related_type ='Contacts'
 LEFT JOIN accounts_contacts ON accounts_contacts.contact_id = contacts.id and accounts_contacts.deleted=0
 LEFT JOIN accounts ON accounts_contacts.account_id = accounts.id and accounts.deleted = 0
 LEFT JOIN email_addr_bean_rel on bean_id = contacts.id and email_addr_bean_rel.deleted=0 AND email_addr_bean_rel.primary_address = 1
@@ -34,7 +39,6 @@ LEFT JOIN email_addresses on email_addr_bean_rel.email_address_id = email_addres
 $cResult = $db->query($cSQL);
 
 // labels of modules
-global $current_language;
 $contactModStrings = return_module_language($current_language, 'Contacts');
 $callModStrings = return_module_language($current_language, 'Calls');
 
@@ -67,15 +71,77 @@ $ctr = 0;
 
 if(!empty($cResult)){
     while($oContact = $db->fetchByAssoc($cResult)){
+
+        $moduleName = 'Contacts';
+  $record = $oContact['contact_id'];
+  $phone_other  =$oContact['phone_other'];
+  $contactname = $oContact['full_name'];
+  $accountname = $oContact['account_name'];
+  $email_address = $oContact['email1'];
+
+  if (empty($logcall_options)) {
+        $logcall_options = array(
+            'log_call' => array('Log Call', 'accept_inline'),
+            'no_result' => array('No Result', 'decline_inline'),
+            'schedule_call' => array('Schedule Call', 'schedule_inline'),
+            'create_note' => array('Create Note', 'txt_image_inline')
+        );
+    }
+
+    $list_view_options = $sugar_config['teleseller'][$moduleName]['listview'];
+    $retStr = "<span style='white-space: nowrap'> <div id='$record'>"; //Encapsulate in span for IE7 to correctly handle nowrap
+
+    foreach ($logcall_options as $config_name => $html_tupple) {
+        if (isset($list_view_options[$config_name]) && $list_view_options[$config_name] == 'true' || $config_name == "appointment") {
+            $icon_log_call_html = SugarThemeRegistry::current()->getImage($html_tupple[1],
+                'align="absmiddle" alt="' . $html_tupple[0] . '" border="0"');
+
+            if ($config_name == "log_call") {
+                $retStr .= "<a id =\"$record\" data-json=\"$json_history\" href=\"javascript:LogCall('$moduleName', '$record', 'null','$phone_other', '$contactname','$accountname');\"" .
+                    ' class="listViewTdToolsS1" title="' . $html_tupple[0] . '"' .
+                    " style='vertical-align:top'>$icon_log_call_html</a>";
+            }
+
+            if ($config_name == "no_result") {
+                $retStr .= "<a href=\"javascript:NoResultCall('$moduleName', '$record');\"" .
+                    ' class="listViewTdToolsS1" title="' . $html_tupple[0] . '"' .
+                    " style='vertical-align:top'>$icon_log_call_html</a>";
+            }
+            if ($config_name == "schedule_call") {
+                $retStr .= "<a href=\"javascript:ScheduleCall('$moduleName', '$record');\"" .
+                    ' class="listViewTdToolsS1" title="' . $html_tupple[0] . '"' .
+                    " style='vertical-align:top'>$icon_log_call_html</a>";
+            }
+            if ($config_name == "create_note") {
+                $retStr .= "<a href=\"javascript:CreateNote('$moduleName', '$record','null');\"" .
+                    ' class="listViewTdToolsS1" title="' . $html_tupple[0] . '"' .
+                    " style='vertical-align:top'>$icon_log_call_html</a>";
+            }
+            if ($config_name == "appointment") {
+                ////$retStr .= "<a href=\"javascript:ManageAppointments('$moduleName', '$record', '$contactname', '$accountname');\"" .
+                $retStr .= "<a href=\"javascript:ManageAppointments('$moduleName', '$record', '$email_address','$contactname', '$accountname');\"" .
+                    ' class="listViewTdToolsS1" title="' . $html_tupple[0] . '"' .
+                    " style='vertical-align:top'>$icon_log_call_html</a>";
+            }
+        }
+    }
+    $retStr .= "</div></span>"; //End span for IE7
+
+
+
+
+
+
+
         $ctr++;
         $class = ($ctr & 1) ? "oddListRowS1" : "evenListRowS1";
         $sTableRow .= '<tr >';
-	if($oContact['account_name'] != ''){
-        $sTableRow .= '<td><a href = '.$url.'#Accounts/'.$oContact['account_id'].'">'.$oContact['account_name'].'</a></td>';
-	}else{
-	$sTableRow .= '<td></td>';
-	}
-	$sTableRow .= '<td><a href = '.$url.'#Contacts/'.$oContact['contact_id'].'">'.$oContact['full_name'].'</a></td>';
+	      if($oContact['account_name'] != ''){
+          $sTableRow .= '<td><a href = '.$url.'#Accounts/'.$oContact['account_id'].'">'.$oContact['account_name'].'</a></td>';
+	      }else{
+	        $sTableRow .= '<td></td>';
+	      }
+	      $sTableRow .= '<td><a href = '.$url.'#Contacts/'.$oContact['contact_id'].'">'.$oContact['full_name'].'</a></td>';
         $sTableRow .= '<td>'.$oContact['title'].'</td>';
         $sTableRow .= '<td>'.$oContact['phone_other'].'</td>';
         $sTableRow .= '<td>'.$oContact['phone_work'].'</td>';
@@ -83,7 +149,11 @@ if(!empty($cResult)){
         $sTableRow .= '<td>'.$oContact['primary_address_city'].'</td>';
         $sTableRow .= '<td>'.$oContact['primary_address_state'].'</td>';
         $sTableRow .= '<td><div id = "reloadVal_'.$oContact['contact_id'].'" name = "reloadVal_'.$oContact['contact_id'].'">'.$oContact['call_outcome_c'].'</div></td>';
-        $sTableRow .= '<td>'.renderLogCall('Contacts', $oContact['contact_id'], $logcall_options).'</td>';
+
+
+
+
+        $sTableRow .= '<td>'.$retStr.'</td>';
         $sTableRow .= '</tr>';
     }
 }
