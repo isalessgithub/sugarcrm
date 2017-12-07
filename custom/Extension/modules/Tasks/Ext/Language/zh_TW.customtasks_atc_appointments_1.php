44dc0c46dfd76be0fa183c711181859effb70ad9
root <root@ip-172-31-53-184.ec2.internal>
2017-12-07 19:51:53 +0000

    dec 7

--- custom/Extension/modules/Tasks/Ext/Language/zh_TW.customtasks_atc_appointments_1.php
+++ custom/Extension/modules/Tasks/Ext/Language/zh_TW.customtasks_atc_appointments_1.php
@@ -4,6 +4,16 @@
 <?php
 // Merged from custom/Extension/modules/Tasks/Ext/Language/zh_TW.customtasks_atc_appointments_1.php
 
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/modules/Tasks/Ext/Language/zh_TW.customtasks_atc_appointments_1.php
+
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/modules/Tasks/Ext/Language/zh_TW.customtasks_atc_appointments_1.php
+
 //THIS FILE IS AUTO GENERATED, DO NOT MODIFY
 $mod_strings['LBL_ATC_ISSCAMPAIGNS_TASKS_1_FROM_ATC_ISSCAMPAIGNS_TITLE'] = 'ISS Campaigns';
 $mod_strings['LBL_ATC_ISSCAMPAIGNS_TASKS_1_FROM_TASKS_TITLE'] = 'ISS Campaigns';
@@ -13,6 +23,36 @@ $mod_strings['LBL_TASKS_CONTACTS_1_FROM_CONTACTS_TITLE'] = 'Contacts';
 $mod_strings['LBL_TASKS_CONTACTS_1_FROM_TASKS_TITLE'] = 'Contacts';
 $mod_strings['LBL_TASKS_ATC_APPOINTMENTS_1_FROM_ATC_APPOINTMENTS_TITLE'] = 'Appointments';
 $mod_strings['LBL_TASKS_ATC_APPOINTMENTS_1_FROM_TASKS_TITLE'] = 'Appointments';
+
+?>
+<?php
+// Merged from custom/Extension/modules/Tasks/Ext/Language/temp.php
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_ATC_ISSCAMPAIGNS_TASKS_1_FROM_ATC_ISSCAMPAIGNS_TITLE'] = 'ISS Campaigns';
+$mod_strings['LBL_ATC_ISSCAMPAIGNS_TASKS_1_FROM_TASKS_TITLE'] = 'ISS Campaigns';
+$mod_strings['LBL_TASKS_ACCOUNTS_1_FROM_ACCOUNTS_TITLE'] = 'Accounts';
+$mod_strings['LBL_TASKS_ACCOUNTS_1_FROM_TASKS_TITLE'] = 'Accounts';
+$mod_strings['LBL_TASKS_CONTACTS_1_FROM_CONTACTS_TITLE'] = 'Contacts';
+$mod_strings['LBL_TASKS_CONTACTS_1_FROM_TASKS_TITLE'] = 'Contacts';
+$mod_strings['LBL_TASKS_ATC_APPOINTMENTS_1_FROM_ATC_APPOINTMENTS_TITLE'] = 'Appointments';
+$mod_strings['LBL_TASKS_ATC_APPOINTMENTS_1_FROM_TASKS_TITLE'] = 'Appointments';
+
+
+?>
+<?php
+// Merged from custom/Extension/modules/Tasks/Ext/Language/temp.php
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_ATC_ISSCAMPAIGNS_TASKS_1_FROM_ATC_ISSCAMPAIGNS_TITLE'] = 'ISS Campaigns';
+$mod_strings['LBL_ATC_ISSCAMPAIGNS_TASKS_1_FROM_TASKS_TITLE'] = 'ISS Campaigns';
+$mod_strings['LBL_TASKS_ACCOUNTS_1_FROM_ACCOUNTS_TITLE'] = 'Accounts';
+$mod_strings['LBL_TASKS_ACCOUNTS_1_FROM_TASKS_TITLE'] = 'Accounts';
+$mod_strings['LBL_TASKS_CONTACTS_1_FROM_CONTACTS_TITLE'] = 'Contacts';
+$mod_strings['LBL_TASKS_CONTACTS_1_FROM_TASKS_TITLE'] = 'Contacts';
+$mod_strings['LBL_TASKS_ATC_APPOINTMENTS_1_FROM_ATC_APPOINTMENTS_TITLE'] = 'Appointments';
+$mod_strings['LBL_TASKS_ATC_APPOINTMENTS_1_FROM_TASKS_TITLE'] = 'Appointments';
+
 
 ?>
 <?php
---
+++ custom/Extension/modules/Tasks/Ext/clients/base/layouts/subpanels/_overridesubpanel-for-tasks_atc_appointments_1.php
@@ -0,0 +1,9 @@
+<?php
+// created: 2017-12-07 12:37:13
+$viewdefs['Tasks']['base']['layout']['subpanels']['components'][] = array (
+  'override_subpanel_list_view' => 
+  array (
+    'link' => 'tasks_atc_appointments_1',
+    'view' => 'subpanel-for-accountsaccounts_atc_appointments_1',
+  ),
+);
