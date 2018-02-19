99ab7e14f23627c4a80b1d912d9d5b66571956b7
Milos Kovacevic <mkovacevic@eontek.co>
2018-02-19 15:57:02 +0100

    add module ‘report delivery configuration’

---
+++ modules/MS_ReportDeliveryConfiguration/language/ar_SA.lang.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$mod_strings = array (
+  'LBL_TEAM' => 'الفرق',
+  'LBL_TEAMS' => 'الفرق',
+  'LBL_TEAM_ID' => 'معرّف الفريق',
+  'LBL_ASSIGNED_TO_ID' => 'معرّف المستخدم المعين',
+  'LBL_ASSIGNED_TO_NAME' => 'تعيين إلى',
+  'LBL_TAGS_LINK' => 'العلامات',
+  'LBL_TAGS' => 'العلامات',
+  'LBL_ID' => 'المعرّف',
+  'LBL_DATE_ENTERED' => 'تاريخ الإنشاء',
+  'LBL_DATE_MODIFIED' => 'تاريخ التعديل',
+  'LBL_MODIFIED' => 'تم التعديل بواسطة',
+  'LBL_MODIFIED_ID' => 'تم التعديل بواسطة المعرّف',
+  'LBL_MODIFIED_NAME' => 'تم التعديل بواسطة الاسم',
+  'LBL_CREATED' => 'تم الإنشاء بواسطة',
+  'LBL_CREATED_ID' => 'تم الإنشاء بواسطة المعرّف',
+  'LBL_DOC_OWNER' => 'مالك المستند',
+  'LBL_USER_FAVORITES' => 'المستخدمون الذي يفضلون',
+  'LBL_DESCRIPTION' => 'الوصف',
+  'LBL_DELETED' => 'تم الحذف',
+  'LBL_NAME' => 'الاسم',
+  'LBL_CREATED_USER' => 'تم الإنشاء بواسطة مستخدم',
+  'LBL_MODIFIED_USER' => 'تم التعديل بواسطة مستخدم',
+  'LBL_LIST_NAME' => 'الاسم',
+  'LBL_EDIT_BUTTON' => 'تحرير',
+  'LBL_REMOVE' => 'إزالة',
+  'LBL_EXPORT_MODIFIED_BY_NAME' => 'تم التعديل بواسطة الاسم',
+  'LBL_LIST_FORM_TITLE' => 'Report Delivery Configurations القائمة',
+  'LBL_MODULE_NAME' => 'Report Delivery Configurations',
+  'LBL_MODULE_TITLE' => 'Report Delivery Configurations',
+  'LBL_MODULE_NAME_SINGULAR' => 'Report Delivery Configuration',
+  'LBL_HOMEPAGE_TITLE' => 'الخاص بي Report Delivery Configurations',
+  'LNK_NEW_RECORD' => 'إنشاء Report Delivery Configuration',
+  'LNK_LIST' => 'عرض Report Delivery Configurations',
+  'LNK_IMPORT_MS_REPORTDELIVERYCONFIGURATION' => 'Import Report Delivery Configurations',
+  'LBL_SEARCH_FORM_TITLE' => 'بحث Report Delivery Configuration',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'عرض السجل',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'سير النشاط الكلي',
+  'LBL_MS_REPORTDELIVERYCONFIGURATION_SUBPANEL_TITLE' => 'Report Delivery Configurations',
+  'LBL_NEW_FORM_TITLE' => 'جديد Report Delivery Configuration',
+  'LNK_IMPORT_VCARD' => 'Import Report Delivery Configuration vCard',
+  'LBL_IMPORT' => 'Import Report Delivery Configurations',
+  'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new Report Delivery Configuration record by importing a vCard from your file system.',
+);
---
+++ modules/MS_ReportDeliveryConfiguration/language/pt_PT.lang.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$mod_strings = array (
+  'LBL_TEAM' => 'Equipas',
+  'LBL_TEAMS' => 'Equipas',
+  'LBL_TEAM_ID' => 'Id Equipa',
+  'LBL_ASSIGNED_TO_ID' => 'ID de Utilizador Atribuído',
+  'LBL_ASSIGNED_TO_NAME' => 'Atribuído a',
+  'LBL_TAGS_LINK' => 'Etiquetas',
+  'LBL_TAGS' => 'Etiquetas',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Data da Criação',
+  'LBL_DATE_MODIFIED' => 'Data da Modificação',
+  'LBL_MODIFIED' => 'Modificado Por',
+  'LBL_MODIFIED_ID' => 'Modificado Por Id',
+  'LBL_MODIFIED_NAME' => 'Modificado Por Nome',
+  'LBL_CREATED' => 'Criado Por',
+  'LBL_CREATED_ID' => 'ID do Utilizador que Criou',
+  'LBL_DOC_OWNER' => 'Proprietário do Documento',
+  'LBL_USER_FAVORITES' => 'Utilizadores que adicionaram aos Favoritos',
+  'LBL_DESCRIPTION' => 'Descrição',
+  'LBL_DELETED' => 'Eliminado',
+  'LBL_NAME' => 'Nome',
+  'LBL_CREATED_USER' => 'Criado por Utilizador',
+  'LBL_MODIFIED_USER' => 'Modificado por Utilizador',
+  'LBL_LIST_NAME' => 'Nome',
+  'LBL_EDIT_BUTTON' => 'Editar',
+  'LBL_REMOVE' => 'Remover',
+  'LBL_EXPORT_MODIFIED_BY_NAME' => 'Modificado Por Nome',
+  'LBL_LIST_FORM_TITLE' => 'Report Delivery Configurations Lista',
+  'LBL_MODULE_NAME' => 'Report Delivery Configurations',
+  'LBL_MODULE_TITLE' => 'Report Delivery Configurations',
+  'LBL_MODULE_NAME_SINGULAR' => 'Report Delivery Configuration',
+  'LBL_HOMEPAGE_TITLE' => 'Minha Report Delivery Configurations',
+  'LNK_NEW_RECORD' => 'Criar Report Delivery Configuration',
+  'LNK_LIST' => 'Vista Report Delivery Configurations',
+  'LNK_IMPORT_MS_REPORTDELIVERYCONFIGURATION' => 'Import Report Delivery Configurations',
+  'LBL_SEARCH_FORM_TITLE' => 'Pesquisar Report Delivery Configuration',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'Ver Histórico',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Fluxo de Atividades',
+  'LBL_MS_REPORTDELIVERYCONFIGURATION_SUBPANEL_TITLE' => 'Report Delivery Configurations',
+  'LBL_NEW_FORM_TITLE' => 'Novo Report Delivery Configuration',
+  'LNK_IMPORT_VCARD' => 'Import Report Delivery Configuration vCard',
+  'LBL_IMPORT' => 'Import Report Delivery Configurations',
+  'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new Report Delivery Configuration record by importing a vCard from your file system.',
+);
