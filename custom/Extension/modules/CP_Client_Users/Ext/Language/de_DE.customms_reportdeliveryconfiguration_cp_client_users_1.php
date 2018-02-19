99ed58e0f25b452d69b3922c59424f2254b9c356
Milos Kovacevic <mkovacevic@eontek.co>
2018-02-19 16:11:33 +0100

    relate report delivery configuration to CP users

---
+++ custom/Extension/modules/CP_Client_Users/Ext/Language/de_DE.customms_reportdeliveryconfiguration_cp_client_users_1.php
@@ -0,0 +1,21 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+?>
+<?php
+// Merged from custom/Extension/modules/CP_Client_Users/Ext/Language/de_DE.customms_reportdeliveryconfiguration_cp_client_users_1.php
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_ATC_ISSCAMPAIGNS_CP_CLIENT_USERS_1_FROM_ATC_ISSCAMPAIGNS_TITLE'] = 'ISS Campaigns';
+$mod_strings['LBL_MS_REPORTDELIVERYCONFIGURATION_CP_CLIENT_USERS_1_FROM_MS_REPORTDELIVERYCONFIGURATION_TITLE'] = 'Report Delivery Configurations';
+$mod_strings['LBL_MS_REPORTDELIVERYCONFIGURATION_CP_CLIENT_USERS_1_FROM_CP_CLIENT_USERS_TITLE'] = 'Report Delivery Configurations';
+
+?>
+<?php
+// Merged from custom/Extension/modules/CP_Client_Users/Ext/Language/temp.php
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_ATC_ISSCAMPAIGNS_CP_CLIENT_USERS_1_FROM_ATC_ISSCAMPAIGNS_TITLE'] = 'ISS Campaigns';
+$mod_strings['LBL_MS_REPORTDELIVERYCONFIGURATION_CP_CLIENT_USERS_1_FROM_MS_REPORTDELIVERYCONFIGURATION_TITLE'] = 'Report Delivery Configurations';
+$mod_strings['LBL_MS_REPORTDELIVERYCONFIGURATION_CP_CLIENT_USERS_1_FROM_CP_CLIENT_USERS_TITLE'] = 'Report Delivery Configurations';
+
+?>
