8a7a0103a560d707c926324fa6421af5999735dd
Milos Kovacevic <mkovacevic@eontek.co>
2018-02-19 16:03:08 +0100

    relate report delivery configuration with log

---
+++ custom/Extension/modules/MS_ReportDeliveryConfiguration/Ext/Vardefs/ms_reportdeliveryconfiguration_ms_reportdeliverylog_1_MS_ReportDeliveryConfiguration.php
@@ -0,0 +1,14 @@
+<?php
+// created: 2018-02-19 15:02:17
+$dictionary["MS_ReportDeliveryConfiguration"]["fields"]["ms_reportdeliveryconfiguration_ms_reportdeliverylog_1"] = array (
+  'name' => 'ms_reportdeliveryconfiguration_ms_reportdeliverylog_1',
+  'type' => 'link',
+  'relationship' => 'ms_reportdeliveryconfiguration_ms_reportdeliverylog_1',
+  'source' => 'non-db',
+  'module' => 'MS_ReportDeliveryLog',
+  'bean_name' => 'MS_ReportDeliveryLog',
+  'vname' => 'LBL_MS_REPORTDELIVERYCONFIGURATION_MS_REPORTDELIVERYLOG_1_FROM_MS_REPORTDELIVERYCONFIGURATION_TITLE',
+  'id_name' => 'ms_reportdaa12uration_ida',
+  'link-type' => 'many',
+  'side' => 'left',
+);
---
+++ custom/Extension/modules/relationships/wirelesslayoutdefs/ms_reportdeliveryconfiguration_ms_reportdeliverylog_1_MS_ReportDeliveryConfiguration.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2018-02-19 15:02:17
+$layout_defs["MS_ReportDeliveryConfiguration"]["subpanel_setup"]['ms_reportdeliveryconfiguration_ms_reportdeliverylog_1'] = array (
+  'order' => 100,
+  'module' => 'MS_ReportDeliveryLog',
+  'subpanel_name' => 'default',
+  'title_key' => 'LBL_MS_REPORTDELIVERYCONFIGURATION_MS_REPORTDELIVERYLOG_1_FROM_MS_REPORTDELIVERYLOG_TITLE',
+  'get_subpanel_data' => 'ms_reportdeliveryconfiguration_ms_reportdeliverylog_1',
+);
