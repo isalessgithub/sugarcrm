32a493e101db627adf2562e36cbe7aabe0630c15
Kovacevic Milos <mkovacevic@eontek.co>
2018-08-10 16:06:16 +0200

    fix campaigns subpanel on accounts

--- custom/modules/ATC_ISSCampaigns/clients/base/views/subpanel-for-accounts-atc_isscampaigns_accounts/subpanel-for-accounts-atc_isscampaigns_accounts.php
+++ custom/modules/ATC_ISSCampaigns/clients/base/views/subpanel-for-accounts-atc_isscampaigns_accounts/subpanel-for-accounts-atc_isscampaigns_accounts.php
@@ -1,5 +1,53 @@
 <?php
-// created: 2018-01-09 19:29:37
+// created: 2018-08-10 14:02:55
 $viewdefs['ATC_ISSCampaigns']['base']['view']['subpanel-for-accounts-atc_isscampaigns_accounts'] = array (
+  'panels' => 
+  array (
+    0 => 
+    array (
+      'name' => 'panel_header',
+      'label' => 'LBL_PANEL_1',
+      'fields' => 
+      array (
+        0 => 
+        array (
+          'default' => true,
+          'label' => 'LBL_NAME',
+          'enabled' => true,
+          'name' => 'name',
+          'link' => true,
+          'type' => 'name',
+        ),
+        1 => 
+        array (
+          'name' => 'campaign_type_c',
+          'label' => 'LBL_CAMPAIGN_TYPE',
+          'enabled' => true,
+          'default' => true,
+        ),
+        2 => 
+        array (
+          'name' => 'appointment_target_campaign_c',
+          'label' => 'LBL_APPOINTMENT_TARGET_CAMPAIGN',
+          'enabled' => true,
+          'default' => true,
+        ),
+        3 => 
+        array (
+          'name' => 'campaign_start_date_c',
+          'label' => 'LBL_CAMPAIGN_START_DATE',
+          'enabled' => true,
+          'default' => true,
+        ),
+        4 => 
+        array (
+          'name' => 'campaign_finish_date_c',
+          'label' => 'LBL_CAMPAIGN_FINISH_DATE',
+          'enabled' => true,
+          'default' => true,
+        ),
+      ),
+    ),
+  ),
   'type' => 'subpanel-list',
 );
